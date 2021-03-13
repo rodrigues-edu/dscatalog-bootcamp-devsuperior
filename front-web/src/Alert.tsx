a36717fdd6a7b6b4cc6314b911e9317179c9a870
Eduardo Rodrigues <silva.rodriguesedu@icloud.com>
2021-03-13 11:10:34 -0300

    CategoryResource

---
+++ front-web/src/Alert.tsx
@@ -0,0 +1,11 @@
+
+type Props = {
+    text: string;
+}
+
+const Alert = ({ text } : Props) => (
+    <div className="alert alert-primary">
+        Hello {text}!
+    </div>
+);
+export default Alert;
---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import Alert from './Alert';
+
+
+
+const App = () => {
+    return (
+        <div className="container mt-5" >
+            <Alert text="Eduardo Rodrigues"/>
+            <Alert text="Carol"/>
+        </div>
+    );
+}
+
+export default App;
